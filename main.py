38cfd44f26ffaba0a8381df098d99f09bbabf8a5
Laktyn <101568300+Laktyn@users.noreply.github.com>
2023-10-24 23:33:17 +0200

    added few functions to files

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from tqdm import tqdm
 from math import floor
+from utilities import evolve, np_to_complex_pt
+from test import test
 
 # cuda 
 
@@ -68,16 +70,18 @@ print("output_dim (phase length) = {}".format(output_dim))
 
 # phase generator to evolve our initial pulse to input pulse
 
-def phase_gen(num, max_order = 10, max_value = None):
-
-    if np.random.uniform(low = 0, high = 1) < 1.0:      # slowly varying phase
+def phase_gen(num, max_order = 10, max_value = None, phase_type = "regular"):
+    '''
+    phase_type = \"regular\" or \"hermitian\"
+    '''
+    if phase_type == "regular":      # slowly varying phase
         X = np.linspace(-1, 1, num)
         Y = np.zeros(num)
         
         for order in range(max_order):
             coef = np.random.uniform(low = -1, high = 1)
             Y += coef*X**order
-    else:                                               # rapidly varying phase  UPDATE: It causes convergence
+    elif phase_type == "hermitian":                                               # rapidly varying phase  UPDATE: It causes convergence
         Y = np.zeros(num)
         for order in range(4):
             coef = np.random.uniform(low = -1, high = 1)
@@ -85,7 +89,9 @@ def phase_gen(num, max_order = 10, max_value = None):
                 bandwidth = [-1, 1],
                 centre = 0,
                 FWHM = 0.5,
-                num = num).Y
+                num = num).Y    
+    else:
+        raise Exception("Undefined phase_type")
     if max_value == None:
         return Y
     else:
@@ -93,57 +99,19 @@ def phase_gen(num, max_order = 10, max_value = None):
 
 you_dont_trust_me_that_these_phases_look_cool = True
 
-if you_dont_trust_me_that_these_phases_look_cool:
-    for i in range(10):
-        phase = phase_gen(100, 10)
-        plt.plot(np.linspace(0, 1, 100), phase, color = "deeppink")
-        plt.grid()
-        plt.title("Test phase")
-        plt.savefig("phase_{}.jpg".format(i + 1))
-        plt.close()
-
-# real-valued numpy array to complex pytorch tensor
-
-def np_to_complex_pt(tens):
-
-    tens = torch.tensor([[tens[i], 0] for i in range(len(tens))], requires_grad = True, device = my_device, dtype = my_dtype)
-    tens = torch.view_as_complex(tens)
-    tens = tens.reshape(1, tens.numel())
-
-    return tens
-
-# evolution by given phase operator
-
-def evolve(intensity, phase, abs = True):
-
-    intensity = torch.fft.fftshift(intensity)
-    intensity = torch.fft.fft(intensity)
-    intensity = torch.fft.fftshift(intensity)
-    
-    long_phase = torch.concat([torch.zeros(size = [floor((input_dim-output_dim)/2)], requires_grad = True, device = my_device, dtype = my_dtype), 
-                          phase,
-                          torch.zeros(size = [floor((input_dim-output_dim)/2)], requires_grad = True, device = my_device, dtype = my_dtype)])
-    
-    complex_intensity = torch.mul(intensity, torch.exp(1j*long_phase))
-
-    complex_intensity = torch.fft.ifftshift(complex_intensity)
-    complex_intensity = torch.fft.ifft(complex_intensity)
-    complex_intensity = torch.fft.ifftshift(complex_intensity)
-
-    if abs:
-        return complex_intensity.abs()
-    else:
-        return complex_intensity
     
 # now function to provide input to the network
 
-def pulse_gen(max_phase_value = None):
+def pulse_gen(max_phase_value = None, phase_type = "regular"):
+
+    from utilities import evolve, np_to_complex_pt
 
     intensity = Y_initial.copy()
     intensity = np_to_complex_pt(intensity)
 
     phase_significant = phase_gen(num = output_dim, 
-                            max_value = np.random.uniform(low = 0, high = max_phase_value))
+                            max_value = max_phase_value,
+                            phase_type = phase_type)
     phase_significant = torch.tensor(phase_significant, requires_grad = True, device = my_device, dtype = my_dtype)
 
     intensity = evolve(intensity, phase_significant)
@@ -183,6 +151,8 @@ elif test_pulse_type == "random_evolution":
 class network(nn.Module):
     def __init__(self, input_size, n, output_size):
         super(network, self).__init__()
+        self.input = input_size
+        self.output = output_size
 
         self.linear_1 = nn.Linear(input_size, n)
         self.linear_2 = nn.Linear(n, n)
@@ -251,87 +221,7 @@ for iter in tqdm(range(iteration_num)):
         else:
             print("Iteration np. {}. Loss {}.".format(iter, np.mean(np.array(loss_list[iter-stat_time: iter]))))
 
-        plot_from = floor(input_dim*1/6)
-        plot_to = floor(input_dim*5/6)
-
-        # generate test chirp pulse
-
-        test_phase_pred = model(test_pulse.abs())
-        test_phase_pred = test_phase_pred.reshape([output_dim])
-
-        # evolve
-
-        initial_intensity = np_to_complex_pt(Y_initial.copy())
-
-        test_intensity = evolve(initial_intensity, test_phase_pred)
-        reconstructed = test_intensity.abs() 
-
-        plt.subplot(1, 2, 1)
-        plt.title("The intensity")
-
-        plt.scatter(initial_pulse.X[plot_from:plot_to], 
-                   np.abs(np.reshape(reconstructed.clone().cpu().detach().numpy(), input_dim)[plot_from:plot_to]), 
-                    color = "green", 
-                    s = 1,
-                    zorder = 10)
-        plt.plot(initial_pulse.X[plot_from:plot_to], 
-                    np.abs(initial_pulse.Y[plot_from:plot_to]), 
-                    linestyle = "dashed", 
-                    color = "black", 
-                    lw = 1,
-                    zorder = 5)
-        plt.fill_between(initial_pulse.X[plot_from:plot_to], 
-                            np.abs(np.reshape(test_pulse.clone().cpu().detach().numpy(), input_dim))[plot_from:plot_to], 
-                            color = "darkviolet", 
-                            alpha = 0.2,
-                            zorder = 0)
-        plt.xlabel("THz")
-        plt.legend(["Reconstructed intensity", "Initial intensity", "Target intensity"], bbox_to_anchor = [0.95, -0.15])
-        plt.grid()
-
-        plt.subplot(1, 2, 2)
-
-        plt.title("The phase")
-
-        reconstructed_phase = np.unwrap(test_phase_pred.clone().cpu().detach().numpy().reshape(output_dim))
-        reconstructed_phase -= reconstructed_phase[floor(output_dim/2)]
-
-        idx_start = floor(input_dim/2 - output_dim/2)
-        idx_end = floor(input_dim/2 + output_dim/2)
-
-        plt.scatter(FT_pulse.X[idx_start: idx_end], 
-                    reconstructed_phase, 
-                    s = 1, 
-                    color = "red",
-                    zorder = 10)
-
-        if test_phase != None:
-            test_phase_np = test_phase.clone().cpu().detach().numpy()
-            test_phase_np -= test_phase_np[floor(output_dim/2)]
-            plt.plot(FT_pulse.X[idx_start: idx_end],
-                        np.real(test_phase_np),
-                        color = "black",
-                        lw = 1,
-                        linestyle = "dashed",
-                        zorder = 5)
-
-        FT_pulse.Y /= np.max(FT_pulse.Y[idx_start: idx_end])
-        if test_phase != None:
-            FT_pulse.Y *= np.max(np.concatenate([np.abs(reconstructed_phase), np.abs(test_phase.clone().detach().cpu().numpy())]))
-        else:
-            FT_pulse.Y *= np.max(np.abs(reconstructed_phase))
-
-        plt.fill_between(FT_pulse.X[idx_start: idx_end], 
-                            np.abs(FT_pulse.Y[idx_start: idx_end]), 
-                            alpha = 0.2, 
-                            color = "orange",
-                            zorder = 0)
-        
-        plt.xlabel("Quasi-time (ps)")
-        if test_phase != None:
-            plt.legend(["Reconstructed phase", "Initial phase", "FT intensity"], bbox_to_anchor = [0.95, -0.15])
-        else:
-            plt.legend(["Reconstructed phase", "FT intensity"], bbox_to_anchor = [0.95, -0.15])
-        plt.grid()
-        plt.savefig("pics/reconstructed{}.jpg".format(iter), bbox_inches = "tight", dpi = 200)
-        plt.close()
+            test(model = model,
+                 test_pulse = test_pulse,
+                 initial_pulse_Y = initial_pulse.Y.copy(),
+                 initial_pulse_X = initial_pulse.X.copy())
