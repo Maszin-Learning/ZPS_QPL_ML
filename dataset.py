4a5d093aed9b8b9e4478c63eabb09abe70b6b4a9
Michal <chrzanowski000@gmail.com>
2023-11-05 23:04:16 +0100

    da

--- dataset.py
+++ dataset.py
@@ -1,12 +1,40 @@
 from dataset_generator import Generator
 from torch.utils.data import Dataset
-class Dataset(Dataset):
+import os
+import numpy as np
 
-    def __init__(self, batch_num, batch_size, initial_intensity, phase_len, device, dtype, max_order=10, max_value=10):
-        self.generator = Generator(batch_num, batch_size, initial_intensity, phase_len, device, dtype, max_order=10, max_value=10)
-        self.num = batch_num*batch_size
-        self.init_intensity = initial_intensity
+    
+class Dataset_train(Dataset):
+    def __init__(self, root ,transform):
+        self.labels = []
+        self.dirName_1 = os.paht.join(root,'data','train_intensity') # path to training data with intensity of spectrum
+        self.dirName_2 = os.paht.join(root,'data','train_phase') # path to training data with pahse used to transformate gause of spectrum
+        self.files_names_intensity = []
+        self.files_names_phase = []
+        
+        self.transform = transform
+        self.load_signals()
+    
+
+    def load_signals(self):
+
+        # create list of paths to images
+        self.files_names_intensity = [os.paht.join(self.dirName_1, f) for f in os.listdir(self.dirName_1) if os.isfile(os.paht.join(self.dirName_1, f))]
+        self.files_names_phase = [os.paht.join(self.dirName_2, f) for f in os.listdir(self.dirName_2) if os.isfile(os.paht.join(self.dirName_2, f))]
+        
+
+
     def __len__(self):
-        return self.num
-    def __getitem__(self, idx):
-        return self.generator.data[idx]
+        return len(self.files_names_intensity)
+
+    def __getitem__(self, index):
+        intensity = np.loadtxt(self.files_names_intensity[index],
+                 delimiter="", dtype=np.float32)
+        phase = np.loadtxt(self.files_names_phase[index],
+                 delimiter="", dtype=np.float32)
+        
+        if self.transform:
+            intensity = self.transform(intensity)
+            phase = self.transform(phase)
+
+        return (intensity, phase)
