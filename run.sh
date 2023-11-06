4f797007375d37d1f62027044333c8a3153272be
Michal <chrzanowski000@gmail.com>
2023-11-06 02:03:04 +0100

    ds

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 echo '1st'
-python main.py -lr 1e-4 -en 50 -bs 64 -pf 3 -ds 10000 -g
+python main.py -lr 5e-5 -en 30 -bs 64 -pf 3 -ds 20000 -g
 echo '2nd'
-python main.py -lr 1e-4 -en 50 -bs 128 -pf 3
+python main.py -lr 5e-5 -en 30 -bs 128 -pf 3
 echo '3rd'
-python main.py -lr 1e-4 -en 50 -bs 256 -pf 3
+python main.py -lr 5e-5 -en 30 -bs 256 -pf 3
 echo '4th'
-python main.py -lr 1e-4 -en 50 -bs 512 -pf 3
+python main.py -lr 5e-5 -en 30 -bs 512 -pf 3
