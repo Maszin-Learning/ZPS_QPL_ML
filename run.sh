1a30a145988e0f27498db90003b80cffb155432c
Michal <chrzanowski000@gmail.com>
2023-11-06 02:50:05 +0100

    ds

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ python main.py -lr 5e-5 -en 30 -bs 1024 -pf 3
 echo '3rd'
 python main.py -lr 5e-5 -en 30 -bs 2048 -pf 3
 echo '4th'
-python main.py -lr 5e-5 -en 30 -bs 4096 -pf 3d
+python main.py -lr 5e-5 -en 30 -bs 4096 -pf 3
