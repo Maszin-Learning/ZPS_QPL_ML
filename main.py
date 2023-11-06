958155908d00bf309d2d1012aa60649180f1c1a8
Michal <chrzanowski000@gmail.com>
2023-11-06 21:13:07 +0100

    ds

--- main.py
+++ main.py
@@ -122,6 +122,7 @@ def main(_learning_rate, _epoch_num, _batch_size , _plot_freq, _dataset_size, _g
                                 )
 
         the_generator.generate_and_save()
+        exit()
     
         
         
