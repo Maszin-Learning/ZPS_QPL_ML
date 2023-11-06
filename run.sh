380533b605cfd1290d05a23eaaa9693e045723c4
Michal <chrzanowski000@gmail.com>
2023-11-06 04:09:04 +0100

    ds

--- run.sh
+++ run.sh
@@ -2,3 +2,6 @@
 python main.py -lr 1e-2 -en 15 -bs 1024 -pf 3 -nn 200
 python main.py -lr 1e-2 -en 15 -bs 1024 -pf 3 -nn 400
 python main.py -lr 1e-2 -en 15 -bs 1024 -pf 3 -nn 50
+python main.py -lr 1e-3 -en 15 -bs 1024 -pf 3 -nn 50
+python main.py -lr 1e-3 -en 15 -bs 1024 -pf 3 -nn 200
+python main.py -lr 1e-3 -en 15 -bs 1024 -pf 3 -nn 400
