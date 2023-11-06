90446bfc16fc4835d56afb0d5282c7a9cc722033
Michal <chrzanowski000@gmail.com>
2023-11-06 01:31:50 +0100

    ds

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
+echo '1st'
 python main.py -lr 1e-4 -en 50 -bs 64 -pf 3 -ds 10000 -g
+echo '2nd'
 python main.py -lr 1e-4 -en 50 -bs 128 -pf 3
+echo '3rd'
 python main.py -lr 1e-4 -en 50 -bs 256 -pf 3
+echo '4th'
 python main.py -lr 1e-4 -en 50 -bs 512 -pf 3
