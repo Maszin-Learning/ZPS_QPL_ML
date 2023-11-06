1cee8c7004c5fff1a38417fae262627f7f6ad3e4
Michal <chrzanowski000@gmail.com>
2023-11-06 02:33:28 +0100

    s

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 echo '1st'
-python main.py -lr 5e-5 -en 30 -bs 64 -pf 3 -ds 20000 -g
+python main.py -lr 5e-5 -en 30 -bs 512 -pf 3 -ds 20000 -g
 echo '2nd'
-python main.py -lr 5e-5 -en 30 -bs 128 -pf 3
+python main.py -lr 5e-5 -en 30 -bs 1024 -pf 3
 echo '3rd'
-python main.py -lr 5e-5 -en 30 -bs 256 -pf 3
+python main.py -lr 5e-5 -en 30 -bs 2048 -pf 3
 echo '4th'
-python main.py -lr 5e-5 -en 30 -bs 512 -pf 3
+python main.py -lr 5e-5 -en 30 -bs 4096 -pf 3d
