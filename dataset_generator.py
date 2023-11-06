00d2713b4bad0c6e7ae21951bc52357a9cbe57db
Michal <chrzanowski000@gmail.com>
2023-11-06 03:50:44 +0100

    ds

--- dataset_generator.py
+++ dataset_generator.py
@@ -42,7 +42,8 @@ class Generator():
             
             for order in range(self.max_order):
                 coef = np.random.uniform(low = -1, high = 1)
-
+                Y += coef*X**order
+                """
                 if np.random.choice(50) == 1:
                     _coef = np.random.uniform(low = -1, high = 1)
                     Y += coef*X**order + _coef * np.sin(X)
@@ -55,7 +56,7 @@ class Generator():
                     Y += coef*X**order * np.sin(X)
                 else:
                     Y += coef*X**order
-                    
+                """
                 """
                 if r == 1:
                     Y += np.tan(X)*coef*X**order
--- run.sh
+++ run.sh
@@ -1,10 +1,4 @@
-
-python main.py -lr 1e-4 -en 10 -bs 1024 -pf 2 -ds 15000 -g 
-python main.py -lr 1e-4 -en 10 -bs 2048 -pf 2
-python main.py -lr 1e-4 -en 10 -bs 4096 -pf 2
-python main.py -lr 1e-4 -en 20 -bs 1024 -pf 3
-python main.py -lr 1e-3 -en 20 -bs 1024 -pf 3
+python main.py -lr 1e-1 -en 20 -bs 1024 -pf 3 -ds 15000 -g
 python main.py -lr 1e-2 -en 20 -bs 1024 -pf 3
-python main.py -lr 1e-5 -en 20 -bs 1024 -pf 3
-python main.py -lr 5e-4 -en 20 -bs 1024 -pf 3
-python main.py -lr 0.5 -en 20 -bs 1024 -pf 3
+python main.py -lr 1e-3 -en 20 -bs 1024 -pf 3
+python main.py -lr 1e-4 -en 20 -bs 1024 -pf 3
