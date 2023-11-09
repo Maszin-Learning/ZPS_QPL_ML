a3f1b2510816bb0c2e1aa9193fd8b2e222859bd8
Michal <chrzanowski000@gmail.com>
2023-11-09 23:37:33 +0100

    ds

--- nets.py
+++ nets.py
@@ -329,8 +329,8 @@ class network_9(nn.Module): #do not work on cpu
         self.output = output_size
 
         self.linear_1 = nn.Linear(74752, n) # change 76 to scalable wersion
-        self.linear_2 = nn.Linear(n, n)
-        self.linear_3 = nn.Linear(n, output_size)
+        self.linear_2 = nn.Linear(n, n/2)
+        self.linear_3 = nn.Linear(n/2, output_size)
         
         #convolutions
         self.conv1d_1 = nn.Conv1d(in_channels=1,
@@ -358,7 +358,7 @@ class network_9(nn.Module): #do not work on cpu
 
         
         
-        self.max_pool1d_1 = nn.MaxPool1d(kernel_size=3,
+        self.avg_pool1d_1 = nn.AvgPool1d(kernel_size=3,
                                          stride=None,
                                          padding=0)
         
@@ -370,27 +370,30 @@ class network_9(nn.Module): #do not work on cpu
         self.normal_3 = nn.LayerNorm(output_size)
         self.tanh = nn.Tanh()
         self.relu = nn.ReLU()
+        self.elu = nn.ELU()
         self.bn_fc_1 = nn.BatchNorm1d(n) #wont work on cpu
         self.bn_fc_1 = nn.BatchNorm1d(n)
-        self.dropout = nn.Dropout(0.4)
+        self.dropout = nn.Dropout(0.1)
         
 
     def forward(self,x):
         x = torch.unsqueeze(x, 1)
         #print(x.shape)
         x = self.conv1d_1(x)
-        x = self.max_pool1d_1(x)
+        x = self.avg_pool1d_1(x)
         x = self.conv1d_2(x)
-        x = self.max_pool1d_1(x)
+        x = self.avg_pool1d_1(x)
         x = self.conv1d_3(x)  
-        x = self.dropout(x)
-        x = self.max_pool1d_1(x)
+        #x = self.dropout(x)
+        x = self.avg_pool1d_1(x)
         x = self.conv1d_4(x)
-        x = self.dropout(x)
+        #x = self.dropout(x)
         #print(x.shape) 
         x = torch.flatten(x, start_dim=1, end_dim=-1)
         #print(x.shape)
-        x = self.relu(self.linear_1(x))
+        x = self.elu(self.linear_1(x))
+        x = self.dropout(x)
+        x = self.elu(self.linear_2(x))
         x = self.bn_fc_1(x)
         x = self.dropout(x)
         x = self.linear_3(x)
