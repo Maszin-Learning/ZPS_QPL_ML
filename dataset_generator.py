resolution issues fixed, but gradient diverges

dataset_generator.py
@@ -182,7 +182,7 @@ class Generator():
                                 FWHM = self.target_metadata[1],
                                 num = len(intensity)).Y
             
-        intensity = intensity/np.sqrt(np.sum(intensity*np.conjugate(intensity)))
+        intensity = intensity/np.sum(np.sqrt(intensity*np.conjugate(intensity)))
         intensity = shift_to_centre(intensity_to_shift = intensity,
                                     intensity_ref = self.initial_intensity)
 

main.py
@@ -16,7 +16,7 @@ import utilities
 from dataset import Dataset
 from torch.utils.data import DataLoader #Dataloader module
 import torchaudio
-from test import create_target_pulse, test, create_initial_pulse
+from test import test, create_initial_pulse
 import torchvision.transforms as transforms  # Transformations and augmentations
 from dataset import Dataset_train
 from dataset_generator import Generator
@@ -116,6 +116,8 @@ def main(_learning_rate,
             my_device = torch.device("cpu")
             print (f"Using {my_device}")
 
+    torch.autograd.set_detect_anomaly(True)
+
     # data type
     my_dtype = torch.float32
     
@@ -127,15 +129,19 @@ def main(_learning_rate,
 
     # metaparameters
 
+    meta = u.Parameters()
+
     bandwidth = [-2500, 2500]   # (ps)
 
     spectral_phase_len = 40     # so, assuming 1.5 GHz of pulse shaper's resolution, we get 60 GHz of bandwidth
     temporal_phase_len = 200    # so, assuming 11 ps of modulator's resolution, we get 2200 ps of bandwidth
 
-    comp_time_resolution = 1        # (ps) to avoid border effects we compute with higher resolution than the one of the modulator's
-    comp_freq_resolution = 0.0001   # (THz) as above
+    meta.comp_time_res = 1          # (ps) to avoid border effects we compute with higher resolution than the one of the modulator's
+    meta.comp_freq_res = 0.0001     # (THz) as above
+    meta.eopm_res = 11              # (ps)
+    meta.pulse_shaper_res = 0.0015  # (THZ)
 
-    time_num = floor((bandwidth[1]-bandwidth[0])/comp_time_resolution)             # number of points in the initial pulse
+    time_num = floor((bandwidth[1]-bandwidth[0])/meta.comp_time_res)             # number of points in the initial pulse
 
     centre_init = 500           # not used if initial signal is exponential
     width_init = 100            # not used if initial signal is exponential
@@ -170,10 +176,10 @@ def main(_learning_rate,
     # we compute resolution stuff to be able later to match phase and intensity
 
     initial_pulse_FT = initial_pulse.inv_fourier(inplace = False)
-    init_freq_resolution = initial_pulse_FT.calc_spacing()
-    increase_freq_res = init_freq_resolution/comp_freq_resolution # at the beginning, we dont control the frequency resolution and later we will want to increase it to given level
+    meta.init_freq_res= initial_pulse_FT.calc_spacing()
+    meta.increase_freq_res = meta.init_freq_res/meta.comp_freq_res # at the beginning, we dont control the frequency resolution and later we will want to increase it to given level
 
-    temp_idx_start = np.searchsorted(initial_pulse.X, initial_pulse.quantile(1e-5, "L1")-10) # extra 10 ps just to be sure; from this index we start multiplication of phase
+    meta.temp_idx_start = np.searchsorted(initial_pulse.X, initial_pulse.quantile(1e-3, "L1")-20) # extra 20 ps just to be sure; from this index we start multiplication of phase
 
     # generate training data
 
@@ -246,8 +252,8 @@ def main(_learning_rate,
     temp_intens_target = temp_intens_target/np.sum(temp_intens_target.clone().detach().cpu().numpy())
 
     spectr_intens_target = u.fourier(temp_intens_target)
-    spectr_intens_target = u.cut(spectr_intens_target, 50/init_freq_resolution) # we leave central 50 GHz, we delete the rest in order to save GPU
-    spectr_intens_target = u.increase_resolution(spectr_intens_target, increase_freq_res, device = my_device, dtype = my_dtype)
+    spectr_intens_target = u.cut(spectr_intens_target, 0.1/meta.init_freq_res) # we leave central 100 GHz, we delete the rest in order to save GPU
+    spectr_intens_target = u.increase_resolution(spectr_intens_target, meta.increase_freq_res, device = my_device, dtype = my_dtype)
     
     # learning loop
 
@@ -257,26 +263,36 @@ def main(_learning_rate,
 
     for epoch in range(_epoch_num):
         for pulse, _ in tqdm(dataloader_train):
-
             temp_phase_pred, spectr_phase_pred = model(pulse)
 
             # we apply temporal phase
-            temp_phase_pred = u.increase_resolution(temp_phase_pred, 11/comp_time_resolution, device = my_device, dtype = my_dtype) # 11 ps is the resolution of EOPM
-            temp_intens_pred = u.multiply_by_phase(initial_intensity_pt, temp_phase_pred, index_start = temp_idx_start, device = my_device, dtype = my_dtype)
+            temp_phase_pred = u.increase_resolution(temp_phase_pred, meta.eopm_res/meta.comp_time_res, device = my_device, dtype = my_dtype) # 11 ps is the resolution of EOPM
+            temp_intens_pred = u.multiply_by_phase(initial_intensity_pt, temp_phase_pred, index_start = meta.temp_idx_start, device = my_device, dtype = my_dtype)
 
             # we apply spectral phase
             spectr_intens_pred = u.fourier(temp_intens_pred)
-            spectr_intens_pred = u.cut(spectr_intens_pred, 50/init_freq_resolution) # we leave central 50 GHz, we delete the rest in order to save GPU
-            spectr_intens_pred = u.increase_resolution(spectr_intens_pred, increase_freq_res, device = my_device, dtype = my_dtype)
-            spectr_phase_pred = u.increase_resolution(spectr_phase_pred, 0.0015/comp_freq_resolution, device = my_device, dtype = my_dtype)  # 1.5 GHz is the resolution of the pulse shaper
+
+            old_length = np.array(spectr_intens_pred.shape)[-1]
+            spectr_intens_pred = u.cut(spectr_intens_pred, 0.1/meta.init_freq_res) # we leave central 100 GHz, we delete the rest in order to save GPU
+            new_length = np.array(spectr_intens_pred.shape)[-1]
+            increase_time_res = old_length/new_length
+
+            spectr_intens_pred = u.increase_resolution(spectr_intens_pred, meta.increase_freq_res, device = my_device, dtype = my_dtype)
+            spectr_phase_pred = u.increase_resolution(spectr_phase_pred, meta.pulse_shaper_res/meta.comp_freq_res, device = my_device, dtype = my_dtype)  # 1.5 GHz is the resolution of the pulse shaper
             spectr_intens_pred = u.multiply_by_phase(spectr_intens_pred, spectr_phase_pred, index_start = floor((spectr_intens_pred.shape[-1]-spectr_phase_pred.shape[-1])/2), device = my_device, dtype = my_dtype)
 
             # and back to time domain
             temp_intens_pred = u.inv_fourier(spectr_intens_pred)
+            temp_intens_pred = u.increase_resolution(temp_intens_pred, increase_time_res, device = my_device, dtype = my_dtype)
             temp_intens_pred = u.cut(temp_intens_pred, np.array(temp_intens_target.shape)[-1])
 
             # calculating back-propagation
-            loss = criterion(temp_phase_pred, spectr_phase_pred, temp_intens_pred, spectr_intens_pred, temp_intens_target, spectr_intens_target) # define fuckers
+            loss = criterion(temp_phase_pred,
+                             spectr_phase_pred, 
+                             temp_intens_pred,
+                             spectr_intens_pred,
+                             temp_intens_target, 
+                             spectr_intens_target)
             
             loss.backward()
             optimizer.step()
@@ -298,11 +314,7 @@ def main(_learning_rate,
                                 device = my_device, 
                                 dtype = my_dtype,
                                 iter_num = epoch,
-                                comp_freq_resolution = comp_freq_resolution,
-                                comp_time_resolution = comp_time_resolution,
-                                increase_freq_res = increase_freq_res,
-                                init_freq_resolution = init_freq_resolution,
-                                temp_idx_start = temp_idx_start,
+                                param = meta,
                                 save = True)                        
             cont_penalty = 0
             print("phase's variation MSE: {}.".format(cont_penalty))

nets.py
@@ -12,12 +12,10 @@ class network_0(nn.Module):
         # super function. It inherits from nn.Module and we can access everything in nn.Module
         super(network_0, self).__init__()
         self.input = input_size
-        self.s_len = spectral_phase_len
-        self.s_len = spectral_phase_len
 
         self.linear_1 = nn.Linear(input_size, n)
-        self.linear_2 = nn.Linear(n, spectral_phase_len) 
-        self.linear_3 = nn.Linear(n, temporal_phase_len) 
+        self.linear_s = nn.Linear(n, spectral_phase_len) 
+        self.linear_t = nn.Linear(n, temporal_phase_len) 
 
         self.sigmoid = nn.Sigmoid()
         self.leakyrelu=nn.LeakyReLU(1, inplace=True)
@@ -26,11 +24,11 @@ class network_0(nn.Module):
     def forward(self, x):
         t = self.normal_1(x)
         t = self.leakyrelu(self.linear_1(t))
-        t = self.linear_2(t)
+        t = self.linear_t(t)
 
         s = self.normal_1(x)
         s = self.leakyrelu(self.linear_1(s))
-        s = self.linear_3(s)
+        s = self.linear_s(s)
 
         return (6*np.pi*self.sigmoid(t), 6*np.pi*self.sigmoid(s))
 

test.py
@@ -17,11 +17,7 @@ def test(model,
          device, 
          dtype, 
          save, 
-         comp_time_resolution,
-         temp_idx_start,
-         init_freq_resolution,
-         increase_freq_res,
-         comp_freq_resolution,
+         param,
          iter_num = 0):
     '''
     # OLD LEGEND
@@ -47,24 +43,42 @@ def test(model,
 
     mse = MSELoss()
 
+    # prepare targets
+
+    temp_intens_target = target_pulse.clone()
+    temp_intens_target = torch.tensor(temp_intens_target, requires_grad = False, device = device, dtype = dtype)  # well, it was a tensor even before, but now we know its properties
+    temp_intens_target = temp_intens_target/np.sum(temp_intens_target.clone().detach().cpu().numpy())
+
+    spectr_intens_target = u.fourier(temp_intens_target)
+    spectr_intens_target = u.cut(spectr_intens_target, 0.1/param.init_freq_res) # we leave central 100 GHz, we delete the rest in order to save GPU
+    spectr_intens_target = u.increase_resolution(spectr_intens_target, param.increase_freq_res, device = device, dtype = dtype)
+
     # generate test chirp pulse
 
     temp_phase_pred, spectr_phase_pred = model(target_pulse)
 
     # we apply temporal phase
-    temp_phase_pred = u.increase_resolution(temp_phase_pred, 11/comp_time_resolution, device = device, dtype = dtype) # 11 ps is the resolution of EOPM
+    temp_phase_pred = u.increase_resolution(temp_phase_pred, param.eopm_res/param.comp_time_res, device = device, dtype = dtype) # 11 ps is the resolution of EOPM
     initial_intensity_pt = u.np_to_complex_pt(initial_pulse.Y, device = device, dtype = dtype)
-    temp_intens_pred = u.multiply_by_phase(initial_intensity_pt, temp_phase_pred, index_start = temp_idx_start, device = device, dtype = dtype)
+    temp_intens_pred = u.multiply_by_phase(initial_intensity_pt, temp_phase_pred, index_start = param.temp_idx_start, device = device, dtype = dtype)
 
     # we apply spectral phase
     spectr_intens_pred = u.fourier(temp_intens_pred)
-    spectr_intens_pred = u.cut(spectr_intens_pred, 50/init_freq_resolution) # we leave central 50 GHz, we delete the rest in order to save GPU
-    spectr_intens_pred = u.increase_resolution(spectr_intens_pred, increase_freq_res, device = device, dtype = dtype)
-    spectr_phase_pred = u.increase_resolution(spectr_phase_pred, 0.0015/comp_freq_resolution, device = device, dtype = dtype)  # 1.5 GHz is the resolution of the pulse shaper
+
+    old_length = np.array(spectr_intens_pred.shape)[-1]
+    spectr_intens_pred = u.cut(spectr_intens_pred, 0.1/param.init_freq_res) # we leave central 100 GHz, we delete the rest in order to save GPU
+    new_length = np.array(spectr_intens_pred.shape)[-1]
+    increase_time_res = old_length/new_length
+    
+    spectr_intens_pred = u.increase_resolution(spectr_intens_pred, param.increase_freq_res, device = device, dtype = dtype)
+    spectr_phase_pred = u.increase_resolution(spectr_phase_pred, param.pulse_shaper_res/param.comp_freq_res, device = device, dtype = dtype)  # 1.5 GHz is the resolution of the pulse shaper
     spectr_intens_pred = u.multiply_by_phase(spectr_intens_pred, spectr_phase_pred, index_start = floor((spectr_intens_pred.shape[-1]-spectr_phase_pred.shape[-1])/2), device = device, dtype = dtype)
+
+    spectr_X = np.array([-50 + param.comp_freq_res*1000*n for n in range(spectr_intens_pred.shape[-1])])# this is in GHz!!!
 
     # and back to time domain
     temp_intens_pred2 = u.inv_fourier(spectr_intens_pred)
+    temp_intens_pred = u.increase_resolution(temp_intens_pred, increase_time_res, device = device, dtype = dtype)
     temp_intens_pred2 = u.cut(temp_intens_pred2, np.array(len(target_pulse)))
     
     # create plots
@@ -81,7 +95,7 @@ def test(model,
 
     plt.subplot(2, 2, 2)
 
-    plt.plot(range(spectr_intens_pred.shape[-1]), np.abs(spectr_intens_pred.clone().detach().cpu().numpy().flatten()), color = "darkorange")
+    plt.plot(spectr_X, np.abs(spectr_intens_pred.clone().detach().cpu().numpy().flatten()), color = "darkorange")
     plt.title("Step 2")
     plt.xlabel("Frequency (THz)")
     plt.ylabel("Normalized intensity")
@@ -94,123 +108,12 @@ def test(model,
     plt.ylabel("Normalized intensity")
     plt.grid()
 
-    plt.subplot(2, 2, 4)
-    plt.title("Step 4")
-    plt.xlabel("Frequency (THz)")
-    plt.ylabel("Normalized intensity")
-    plt.grid()
-
     if save:
         if not os.path.isdir("pics"):
             os.mkdir("pics")
         plt.savefig("pics/reconstructed_{}.svg".format(iter_num), bbox_inches = "tight", dpi = 200)
 
     return plt, 0
-    
-
-def create_target_pulse(pulse_type, initial_pulse, phase_len, device, dtype):
-    '''
-    ## Create a target_intensity within given rules.
-    # Arguments:
-
-    pulse_type - if \"hermite\", then the target intensity is a 1 Hermite-Gauss polynomial.
-    If \"chirp\", then the target intensity is chirped Gaussian function. 
-    If \"from_dataset\", then chooses at random a intensity saved in \"data/train_intensity\".
-    If \"two_pulses\", then returns two separated gaussian pulses.
-
-    initial_pulse - a spectrum class object containing the initial spectrum that is - possibly - transformed into target_pulse.
-
-    phase_len - the length of significant part of the Fourier transformed initial_pulse
-
-    # Returns:
-    "target_pulse" being one-dimensional complex PyTorch Tensor.
-    '''
-
-    if pulse_type == "hermite":
-        target_pulse_ = sa.hermitian_pulse(pol_num = 1,
-                                        bandwidth = (initial_pulse.X[0], initial_pulse.X[-1]),
-                                        centre = 500,
-                                        FWHM = 100,
-                                        num = len(initial_pulse),
-                                    x_type = "time")
-
-        target_pulse_.Y = target_pulse_.Y / np.sqrt(np.sum(target_pulse_.Y*np.conjugate(target_pulse_.Y)))
-        target_pulse_.Y = target_pulse_.Y * np.sqrt(np.sum(initial_pulse.Y*np.conjugate(initial_pulse.Y)))
-        
-        target_pulse_.very_smart_shift(target_pulse_.comp_center(norm = "L2")-initial_pulse.comp_center(norm = "L2"))
-        target_pulse_ = np_to_complex_pt(target_pulse_.Y, device = device, dtype = dtype)
-
-    elif pulse_type == "chirp":
-        if dtype == torch.float32:
-            new_dtype = np.float32
-        else:
-            new_dtype = dtype
-        initial_intensity = initial_pulse.Y.copy()
-        chirp = 100
-        transform_phase = chirp*np.linspace(-1, 1, phase_len, dtype = new_dtype)**2
-        target_pulse_ = evolve_np(initial_intensity, transform_phase, dtype = new_dtype)
-
-        target_pulse_ = shift_to_centre(target_pulse_, initial_pulse.Y)
-        target_pulse_ = np_to_complex_pt(target_pulse_, device = device, dtype = torch.float32)
-
-    elif pulse_type == "two_pulses":
-        pulses = sa.hermitian_pulse(pol_num = 0, 
-                                    bandwidth = [initial_pulse.X[0], initial_pulse.X[-1]],
-                                    centre = initial_pulse.quantile(0.5),
-                                    FWHM = initial_pulse.FWHM(),
-                                    num = len(initial_pulse),
-                                    x_type = initial_pulse.x_type)
-        pulses.Y = pulses.Y + pulses.very_smart_shift(-0.5, inplace = False).Y + pulses.very_smart_shift(0.5, inplace = False).Y
-        pulses.Y = pulses.Y / np.sqrt(np.sum(pulses.Y*np.conjugate(pulses.Y)))
-        pulses.Y = pulses.Y * np.sqrt(np.sum(initial_pulse.Y*np.conjugate(initial_pulse.Y)))
-
-        target_pulse_.very_smart_shift(target_pulse_.comp_center()-initial_pulse.comp_center())
-        target_pulse_ = np_to_complex_pt(pulses.Y, device = device, dtype = dtype)
-
-    elif pulse_type == "from_dataset":
-        intensity_labels = os.listdir('data/train_intensity')
-        phase_labels = os.listdir('data/train_phase')
-        dataset_size = len(intensity_labels)
-        number = np.random.randint(low = 0, high = dataset_size)
-        intensity_name = intensity_labels[number]
-        phase_name = phase_labels[number]
-
-        target_pulse_ = np.loadtxt('data/train_intensity/' + intensity_name,
-                 delimiter = " ", dtype = np.float32)
-        transform_phase = np.loadtxt('data/train_phase/' + phase_name,
-                 delimiter = " ", dtype = np.float32)
-        
-        target_pulse_ = shift_to_centre(target_pulse_, initial_pulse.Y)
-        target_pulse_ = np_to_complex_pt(target_pulse_, device = device, dtype = dtype)
-
-    elif pulse_type == "exponential":
-        exp_intensity = np.flip(np.exp(np.linspace(-3, 3, len(initial_pulse))) - np.exp(-3))
-        for i in range(0, floor(len(exp_intensity)*1/3)):
-            exp_intensity[i] = 0
-        exp_intensity = exp_intensity / np.sqrt(np.sum(exp_intensity*np.conjugate(exp_intensity)))
-        exp_intensity = exp_intensity * np.sqrt(np.sum(initial_pulse.Y*np.conjugate(initial_pulse.Y)))
-
-        #exp_intensity = shift_to_centre(exp_intensity, initial_pulse.Y)
-        target_pulse_ = np_to_complex_pt(exp_intensity, device = device, dtype = dtype)
-
-    elif pulse_type == "gauss":
-        target_pulse_ = sa.hermitian_pulse(pol_num = 0,
-                                    bandwidth = (initial_pulse.X[0], initial_pulse.X[-1]),
-                                    centre = 500,
-                                    FWHM = 200,
-                                    num = len(initial_pulse),
-                                    x_type = "time")
-
-        target_pulse_.Y = target_pulse_.Y / np.sqrt(np.sum(target_pulse_.Y*np.conjugate(target_pulse_.Y)))
-        target_pulse_.Y = target_pulse_.Y * np.sqrt(np.sum(initial_pulse.Y*np.conjugate(initial_pulse.Y)))
-
-        target_pulse_.smart_shift(-target_pulse_.comp_center(norm = "L2")+initial_pulse.comp_center(norm = "L2"))
-        target_pulse_ = np_to_complex_pt(target_pulse_.Y, device = device, dtype = dtype)
-
-    else:
-        raise Exception("Pulse_type not defined.")
-
-    return target_pulse_.clone()
 
 
 def create_initial_pulse(bandwidth, centre, FWHM, num, pulse_type):

utilities.py
@@ -242,8 +242,8 @@ def shift_to_centre(intensity_to_shift, intensity_ref):
     num = len(intensity_ref)
 
     x_axis = np.linspace(1, 2, num) # doesn't matter what we take here; I just want to create spectrum
-    spectrum_to_shift = sa.spectrum(x_axis, intensity_to_shift, "freq", "intensity")
-    spectrum_ref = sa.spectrum(x_axis, intensity_ref, "freq", "intensity")
+    spectrum_to_shift = sa.spectrum(x_axis, intensity_to_shift, "THz", "intensity")
+    spectrum_ref = sa.spectrum(x_axis, intensity_ref, "THz", "intensity")
 
     com_s = spectrum_to_shift.comp_center(norm = "L2")
     com_r = spectrum_ref.comp_center(norm = "L2")
@@ -332,7 +332,7 @@ def multiply_by_phase(intensity, phase, index_start, device, dtype):
     zeroes_shape_right[-1] = np.array(intensity.shape)[-1] - index_start - np.array(phase.shape)[-1]
 
     if zeroes_shape_right[-1] < 0:
-        raise Exception("Cannot multiply intensity by the phase, because phase is longer than intensity.")
+        raise Exception("Cannot multiply intensity by the phase ({}), because it is longer than intensity ({}).".format(phase.shape[-1], intensity.shape[-1]))
     
     zeroes_shape_left = tuple(zeroes_shape_left)
     zeroes_shape_right = tuple(zeroes_shape_right)
@@ -375,3 +375,28 @@ def inv_fourier(tensor):
     tensor = torch.fft.ifftshift(tensor)
     tensor = tensor*tensor.abs()
     return tensor
+
+class Parameters:
+    '''
+    ## Storage for some metaparameters.
+
+    comp_time_resolution - time domain resolution used for computation. It's much higher than the real one to avoid numerical errors.
+
+    comp_freq_resolution - frequency domain resolution used for computation. Higher than the \"physical\" one.
+
+    init_freq_resolution - frequency domain resolution determined by the signal properties in time. Cannot be arbitrary defined.
+
+    temp_idx_start - index of the \"first\" non-zero intensity point in the time domain with respect to comp_time_resolution. Starting
+    with this index we multiply intensity by the phase.
+
+    increase_freq_res - comp_freq_resolution/init_freq_resolution
+    '''
+
+    def __init__(self):
+        self.comp_time_res = None 
+        self.temp_idx_start = None
+        self.init_freq_res = None
+        self.increase_freq_res = None
+        self.comp_freq_res = None
+        self.eopm_res = None
+        self.pulse_shaper_res = None