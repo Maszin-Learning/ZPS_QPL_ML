abs -> soft abs fixed the gradients

main.py
@@ -263,15 +263,15 @@ def main(_learning_rate,
 
     for epoch in range(_epoch_num):
         for pulse, _ in tqdm(dataloader_train):
+
             temp_phase_pred, spectr_phase_pred = model(pulse)
 
             # we apply temporal phase
             temp_phase_pred = u.increase_resolution(temp_phase_pred, meta.eopm_res/meta.comp_time_res, device = my_device, dtype = my_dtype) # 11 ps is the resolution of EOPM
-            temp_intens_pred = u.multiply_by_phase(initial_intensity_pt, temp_phase_pred, index_start = meta.temp_idx_start, device = my_device, dtype = my_dtype)
+            temp_intens_pred = u.multiply_by_phase(initial_intensity_pt.clone(), temp_phase_pred, index_start = meta.temp_idx_start, device = my_device, dtype = my_dtype)
 
             # we apply spectral phase
             spectr_intens_pred = u.fourier(temp_intens_pred)
-
             old_length = np.array(spectr_intens_pred.shape)[-1]
             spectr_intens_pred = u.cut(spectr_intens_pred, 0.1/meta.init_freq_res) # we leave central 100 GHz, we delete the rest in order to save GPU
             new_length = np.array(spectr_intens_pred.shape)[-1]
@@ -282,18 +282,20 @@ def main(_learning_rate,
             spectr_intens_pred = u.multiply_by_phase(spectr_intens_pred, spectr_phase_pred, index_start = floor((spectr_intens_pred.shape[-1]-spectr_phase_pred.shape[-1])/2), device = my_device, dtype = my_dtype)
 
             # and back to time domain
-            temp_intens_pred = u.inv_fourier(spectr_intens_pred)
-            temp_intens_pred = u.increase_resolution(temp_intens_pred, increase_time_res, device = my_device, dtype = my_dtype)
-            temp_intens_pred = u.cut(temp_intens_pred, np.array(temp_intens_target.shape)[-1])
+            temp_intens_pred_2 = u.inv_fourier(spectr_intens_pred)
+            temp_intens_pred_2 = u.increase_resolution(temp_intens_pred_2, increase_time_res, device = my_device, dtype = my_dtype)
+            temp_intens_pred_2 = u.cut(temp_intens_pred_2, np.array(temp_intens_target.shape)[-1])
+
 
             # calculating back-propagation
             loss = criterion(temp_phase_pred,
                              spectr_phase_pred, 
-                             temp_intens_pred,
+                             temp_intens_pred_2,
                              spectr_intens_pred,
                              temp_intens_target, 
                              spectr_intens_target)
-            
+ 
+
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
@@ -327,7 +329,6 @@ def main(_learning_rate,
             wandb.log({"chart": fig})
             print('test_loss',test_loss)
             wandb.log({"test_loss": test_loss})
-            fig.close()
 
             model.train()
 

test.py
@@ -83,37 +83,36 @@ def test(model,
     
     # create plots
 
-    plt.figure(figsize = (10, 10), constrained_layout = True)
+    fig, axes = plt.subplots(2, 2, figsize=(10, 10), constrained_layout=True)
 
-    plt.subplot(2, 2, 1)
+    # plot 1
+    axes[0, 0].plot(initial_pulse.X, initial_pulse.Y, color="darkviolet")
+    axes[0, 0].set_title("Step 1")
+    axes[0, 0].set_xlabel("Time (ps)")
+    axes[0, 0].set_ylabel("Normalized intensity")
+    axes[0, 0].grid()
 
-    plt.plot(initial_pulse.X, initial_pulse.Y, color = "darkviolet")
-    plt.title("Step 1")
-    plt.xlabel("Time (ps)")
-    plt.ylabel("Normalized intensity")
-    plt.grid()
+    # plot 2
+    axes[0, 1].plot(spectr_X, np.abs(spectr_intens_pred.clone().detach().cpu().numpy().flatten()), color="darkorange")
+    axes[0, 1].set_title("Step 2")
+    axes[0, 1].set_xlabel("Frequency (THz)")
+    axes[0, 1].set_ylabel("Normalized intensity")
+    axes[0, 1].grid()
 
-    plt.subplot(2, 2, 2)
+    # plot 3
+    axes[1, 0].plot(range(temp_intens_pred2.shape[-1]), np.abs(temp_intens_pred2.clone().detach().cpu().numpy().flatten()), color="darkviolet")
+    axes[1, 0].set_title("Step 3")
+    axes[1, 0].set_xlabel("Time (ps)")
+    axes[1, 0].set_ylabel("Normalized intensity")
+    axes[1, 0].grid()
 
-    plt.plot(spectr_X, np.abs(spectr_intens_pred.clone().detach().cpu().numpy().flatten()), color = "darkorange")
-    plt.title("Step 2")
-    plt.xlabel("Frequency (THz)")
-    plt.ylabel("Normalized intensity")
-    plt.grid()
-
-    plt.subplot(2, 2, 3)
-    plt.plot(range(temp_intens_pred2.shape[-1]), np.abs(temp_intens_pred2.clone().detach().cpu().numpy().flatten()), color = "darkviolet")
-    plt.title("Step 3")
-    plt.xlabel("Time (ps)")
-    plt.ylabel("Normalized intensity")
-    plt.grid()
-
+    # save the figure if needed
     if save:
         if not os.path.isdir("pics"):
             os.mkdir("pics")
-        plt.savefig("pics/reconstructed_{}.svg".format(iter_num), bbox_inches = "tight", dpi = 200)
+        fig.savefig(f"pics/reconstructed_{iter_num}.svg", bbox_inches="tight", dpi=200)
 
-    return plt, 0
+    return fig, 0
 
 
 def create_initial_pulse(bandwidth, centre, FWHM, num, pulse_type):

utilities.py
@@ -339,7 +339,8 @@ def multiply_by_phase(intensity, phase, index_start, device, dtype):
 
     long_phase = torch.concat([torch.zeros(size = zeroes_shape_left, requires_grad = True, device = device, dtype = dtype), 
                           phase,
-                          torch.zeros(size = zeroes_shape_right, requires_grad = True, device = device, dtype = dtype)], dim=phase.ndim-1)
+                          torch.zeros(size = zeroes_shape_right, requires_grad = True, device = device, dtype = dtype)], 
+                          dim=phase.ndim-1)
 
     return torch.mul(intensity, torch.exp(1j*long_phase))
 
@@ -358,23 +359,27 @@ def cut(pt_array, num):
     else:
         raise Exception("WTF, the shape of this tensor is wild.")
     
+def soft_abs(x, epsilon=1e-6):
+    return torch.sqrt(x**2 + epsilon)
+    
 def fourier(tensor):
-    tensor = tensor.clone()
-    tensor = torch.mul(torch.sqrt(tensor.abs()), torch.exp(1j*tensor.angle()))
-    tensor = torch.fft.fftshift(tensor)
-    tensor = torch.fft.fft(tensor, norm = "ortho")
-    tensor = torch.fft.fftshift(tensor)
-    tensor = tensor*tensor.abs()
-    return tensor
+    tensor2 = tensor.clone()
+    tensor2 = torch.mul(torch.sqrt(soft_abs(tensor2)), torch.exp(1j*tensor2.angle()))
+    tensor2 = torch.fft.fftshift(tensor2)
+    tensor2 = torch.fft.fft(tensor2, norm = "ortho")
+    tensor2 = torch.fft.fftshift(tensor2)
+    tensor2 = tensor2*soft_abs(tensor2)
+    return tensor2
 
 def inv_fourier(tensor):
-    tensor = tensor.clone()
-    tensor = torch.mul(torch.sqrt(tensor.abs()), torch.exp(1j*tensor.angle()))
-    tensor = torch.fft.ifftshift(tensor)
-    tensor = torch.fft.ifft(tensor, norm = "ortho")
-    tensor = torch.fft.ifftshift(tensor)
-    tensor = tensor*tensor.abs()
-    return tensor
+    tensor2 = tensor.clone()
+    tensor2 = torch.mul(torch.sqrt(soft_abs(tensor2)), torch.exp(1j*tensor2.angle()))
+    tensor2 = torch.fft.ifftshift(tensor2)
+    tensor2 = torch.fft.ifft(tensor2, norm = "ortho")
+    tensor2 = torch.fft.ifftshift(tensor2)
+    tensor2 = tensor2*soft_abs(tensor2)
+    return tensor2
+
 
 class Parameters:
     '''
@@ -400,3 +405,27 @@ class Parameters:
         self.comp_freq_res = None
         self.eopm_res = None
         self.pulse_shaper_res = None
+
+def plot(tensor):
+    X = np.array(range(np.array(tensor.shape)[-1]))
+    Y = np.abs(tensor.clone().detach().cpu().numpy())
+    plt.scatter(X, Y, color = "red", s = 1)
+    plt.show()
+
+def plot2(tensor1, tensor2):
+    X = np.array(range(np.array(tensor1.shape)[-1]))
+    Y1 = np.abs(tensor1.clone().detach().cpu().numpy())
+    Y2 = np.abs(tensor2.clone().detach().cpu().numpy())
+    plt.scatter(X, Y1, color = "red", s = 1)
+    plt.scatter(X, Y2, color = "blue", s = 1)
+    plt.show()
+
+def print_gradient_after(tensor, name = "So far"):
+    """
+    Registers a backward hook to log gradient norms after backpropagation.
+    """
+    def hook_fn(grad):
+        grad_norm = grad.abs().max().item()
+        print(f"{name} | Max Gradient Norm: {grad_norm}")
+    print(" Computing gradient norm...")
+    tensor.register_hook(hook_fn)